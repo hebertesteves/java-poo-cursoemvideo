2a040c419d5920a3fc803d7e759e8a403cb147ce
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-07-20 13:50:58 -0300

    feat: adiciona códigos da aula 12 (Animal, Ave, Mamifero, Reptil, Peixe, Arara, Cachorro, Canguru, Cobra, Goldfish, Tartaruga, Aula12)

---
+++ src/aula12/Mamifero.java
@@ -0,0 +1,37 @@
+package aula12;
+
+public class Mamifero extends Animal {
+    private String corPelo;
+
+    public Mamifero(double peso, int idade, int membros, String corPelo) {
+        super(peso, idade, membros);
+        this.corPelo = corPelo;
+    }
+
+    public Mamifero() {
+        this.corPelo = "";
+    }
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+
+    @Override
+    public void locomover() {
+        System.out.println("Correndo");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Mamando");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Mamífero");
+    }
+}
